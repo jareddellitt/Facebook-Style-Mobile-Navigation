791089466f035bc5c02075ecda363f0add8743c6
Jared Dellitt <jared@dellitt.com>
2012-02-08 22:09:25 -0600

    changes for when javascript isn't enabled - also styling changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-$(function () {
-	var $content = $("#content");
+(function (doc, $) {
+	var $content = $('#content'),
+		$doc = $(doc);
 
 	function toggle(e) {
 		e.preventDefault();
 		e.stopPropagation();
 
-		$('#content').toggleClass('menu-visible');
+		$content.toggleClass('menu-visible');
 	}
 
+	$('#nav').prependTo($content).css({
+		height: $doc.height()
+	});
+
 	$('#activator').bind('touchstart, click', toggle);
-});
+
+}(document, jQuery));
