10a5e3268135086116ec60a953ac2665c754fc47
Jared Dellitt <jared@dellitt.com>
2012-12-26 15:01:44 -0600

    changing how the nav is moved to prevent flash on page load, adding web app meta tags

--- js/script.js
+++ js/script.js
@@ -3,25 +3,34 @@
         $body = $('body'),
         $content = $('#content'),
         $main = $('#main'),
-        $nav = $('#nav');	
+        $nav = $('#nav'),
+        winHeight = $(window).height(),
+        navMooved = false;
+
+    function moveNav() {
+        $nav.prependTo($body).addClass('visible');
+        navMooved = true;
+    }
 
 	$('#activator').on(Modernizr.touch ? 'touchend' : 'click', function (e) {
 		e.preventDefault();
         e.stopPropagation();
 		
+        if (!navMooved) {
+            moveNav();
+        }
+
         $main.toggleClass('slid-right');
-        $nav.toggleClass('scroll');
 	});
 
     $main.on('webkitTransitionEnd transitionend oTransitionEnd', function (e) {
         $html.toggleClass('no-scrolling');
         $body.toggleClass('no-scrolling');
+        $nav.toggleClass('scroll');
     });
 
-    $nav.prependTo($('body'));
-
-    if ($content.height() < $nav.height()) {
-        $content.height($nav.height());
+    if ($content.height() < winHeight) {
+        $content.height(winHeight);
     }
 
 }($));
